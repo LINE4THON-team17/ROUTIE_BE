bd5f2523d0a2e6d433bab49db54d574c53ead226
이채민 <chaemin@ichaemin-ui-MacBookAir.local>
2025-11-09 01:41:08 +0900

    🎉 Initial commit: setup ROUTIE_BE project

---
+++ src/main/java/com/example/routie_be/RoutieBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.routie_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutieBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RoutieBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/routie_be/RoutieBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.routie_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutieBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
