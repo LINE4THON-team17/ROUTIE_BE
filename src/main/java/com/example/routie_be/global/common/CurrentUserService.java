a9b1199e79010ec0a7569532ae818adc7641f8e6
Chaemin Lee <dlcoals7047@skuniv.ac.kr>
2025-11-13 13:56:25 +0900

    :wrench:fix: currentuserservice

--- src/main/java/com/example/routie_be/global/common/CurrentUserService.java
+++ src/main/java/com/example/routie_be/global/common/CurrentUserService.java
@@ -1,11 +1,27 @@
 package com.example.routie_be.global.common;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import com.example.routie_be.security.UserPrincipal;
 
 @Service
 public class CurrentUserService {
-    // JWT 붙기 전까지는 임시 userId=1 하드코딩
+
     public Long getUserId() {
-        return 1L;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new RuntimeException("인증 정보(User ID)를 가져올 수 없습니다. 유효한 토큰인지 확인하세요.");
+        }
+
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof UserPrincipal userPrincipal) {
+            return userPrincipal.id();
+        }
+
+        throw new RuntimeException("인증 정보(User ID)를 가져올 수 없습니다. 유효한 토큰인지 확인하세요.");
     }
 }
